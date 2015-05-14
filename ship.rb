33381971b1b80fd9f892d2299167613008d5e47c
Julie Torres <reeddetorres@gmail.com>
2015-05-14 19:40:22 -0400

    Commit before my computer dies

--- ship.rb
+++ ship.rb
@@ -1,6 +1,7 @@
 class Ship
   def initialize(length)
     @length = length
+    @ship_placed = []
   end
 
   def length
@@ -8,6 +9,23 @@ class Ship
   end
 
   def place(x, y, horizontal)
-    true
+    return false unless @ship_placed.empty?
+    if horizontal == true
+      tail = x + @length -1
+      (x..tail).map {|x| @ship_placed << [x, y] }
+      return @ship_placed
+    else
+      tail = y + @length -1
+      (y..tail).map {|y| @ship_placed << [x, y] }
+      return @ship_placed
+    end
+  end
+
+  def covers?(x, y)
+    @ship_placed.include?([x, y])
+  end
+
+  def overlaps_with?(ship)
+    #map each of the coordinates from @ship_placed & compare to ship.covered?(for the 2nd ship)
   end
 end
